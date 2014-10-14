8131cb6ed8ea418b499d1f3650928d5ceacd3b17
Michał Moskała <moskala.michal@gmail.com>
2014-10-14 22:49:30 +0200

    Simple login script.

---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $dbc = mysqli_connect('localhost', 'root', 'root', 'ZaawansowaneTechnologieWebowe1') or die ('Error connecting to MySQL server');
+    $query = "SELECT * FROM users WHERE username = '$username' AND password = SHA('$password')";
+
+    $result = mysqli_query($dbc, $query) or die(mysqli_error($dbc));
+    if (mysqli_num_rows($result) > 0) {
+        header('location:index.html');
+    } else {
+        header('location:login.html');
+    }
+
+?>
